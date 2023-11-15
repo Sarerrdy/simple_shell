52fe9e4ccb4e353cd9dc30f1246c69eef415f5eb
Michemor <yatoijema@gmail.com>
2023-11-16 00:02:44 +0300

    Fixed betty compliant errors

--- shell.h
+++ shell.h
@@ -13,28 +13,22 @@
 #include <stdarg.h>
 #include <signal.h>
 
-void interactive_mode();
-void non_interactive_mode();
+void interactive_mode(void);
+void non_interactive_mode(void);
 extern char **environ;
-
 int str_len(char *str);
 char *read_stream(void);
 char *myread_line();
 char **split_string(char *usr_input, const char *delimiter);
 int execute_cmd(char **args);
-
 int get_path_index(char *str);
 char *_getpath(char *pathname);
 void Print_char(char *str);
 int _putchar(char c);
-
-
 char *_strncpy(char *dest, char *src);
 int _strcmp(char *str1, char *str2);
-
-void get_all_env();
-void exit_shell();
-
+void get_all_env(void);
+void exit_shell(void);
 char *search_directories(char **path_tokens, char *command);
 char *build_path(char *directory, char *command);
-#endif
+#endif /* SHELL_H */
